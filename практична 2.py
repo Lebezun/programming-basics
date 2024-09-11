023aed4bdf4e275cc9f2fd36d84dd5eac6903cf9
Максим Лебезун <tassadarromg@gmail.com>
2024-09-11 20:07:24 +0300

    Update практична 2.py

--- практична 2.py
+++ практична 2.py
@@ -7,5 +7,7 @@ list2=[type(text), type(name), type(age)]
 print(list2)
 if type(text) == type(name) == type(age):
     print("Тип str, повторюється найчастіше")
+elif type(text) == type(name) and type(name) == type(age) and type(text) == type(age):
+    print("Тип str, повторюється найчастіше")
 else:
     print("Дані незбігаються")
