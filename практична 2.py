e66995da1b1df317a29880bdea307b39a018b56e
Максим Лебезун <tassadarromg@gmail.com>
2024-09-22 12:15:00 +0300

    практична 2.py

--- практична 2.py
+++ практична 2.py
@@ -1,13 +1,30 @@
-text = "Hello world"
-name = "Максим Лебезун"
-age = 16
-list1=[text, name, age]
-print(list1)
-list2=[type(text), type(name), type(age)]
-print(list2)
-if type(text) == type(name) == type(age):
-    print("Тип str, повторюється найчастіше")
-elif type(text) == type(name) and type(name) == type(age) and type(text) == type(age):
-    print("Тип str, повторюється найчастіше")
-else:
-    print("Дані незбігаються")
+a = 'Максим'
+b = 'Лебезун'
+c = 16
+A = type(a)
+B = type(b)
+C = type(c)
+list1 = [a, b, c]
+list2 = [A, B, C]
+list_string = []
+list_int = []
+
+if A == str:
+    list_string.append(A)
+elif A == int:
+    list_int.append(A)
+
+if B == str:
+    list_string.append(B)
+elif B == int:
+    list_int.append(B)  
+
+if C == str:
+    list_string.append(C)
+elif C == int:
+    list_int.append(C)
+
+if len(list_string) > len(list_int):
+    print('Тип даних str переважає')
+elif len(list_int) > len(list_string):
+    print('Тип даних int переважає')
