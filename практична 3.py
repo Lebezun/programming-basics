f07103df9e32a7181d9b2880faa5043d705c21d8
Максим Лебезун <tassadarromg@gmail.com>
2024-10-15 17:10:21 +0300

    Add files via upload

---
+++ практична 3.py
@@ -0,0 +1,17 @@
+kratos = {
+    "name": "Kratos",
+    "age": 3000,
+    "address": {
+        "city": "Sparta",
+        "street": "there is no",
+        "weapon 1":"Leviathan Axe",
+        "weapon 2": "Blades of Chaos"},
+	 "weight_kg": 136 }
+type_items = {}
+for k, v in kratos.items():
+    if type(v) == dict:
+        for k1, v1 in v.items():
+            type_items[k1] = type(v1)
+    else:
+        type_items[k] = type(v)
+print(type_items)
