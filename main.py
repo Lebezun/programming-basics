5a8e2ae9a9fe6c9947732c5bd5128bb6390cf68c
Максим Лебезун <tassadarromg@gmail.com>
2025-02-10 17:52:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,22 @@
-import os
-import telebot
-from dotenv import load_dotenv
-from api import get_movies_by_genre
-from handlers import send_welcome, handle_unknown as handle_unknown_message
-from decorators import log_message
+class Maksym:
+    def __init__(self, first_name=None, last_name=None, birth_year=None):
+        self.first_name = first_name if first_name is not None else None
+        self.last_name = last_name if last_name is not None else None
+        self.birth_year = birth_year if birth_year is not None else None
 
-load_dotenv('token.env')
+    def get_course(self):
+        if self.birth_year is None:
+            return None
+        current_year = 2025
+        start_year = 2023
+        course = current_year - start_year
+        return min(course, 4)  
 
-TOKEN = os.getenv('TOKEN')
-bot = telebot.TeleBot(TOKEN)
+    def get_name_list(self):
+        return [self.first_name, self.last_name]
 
-def main():
-    @bot.message_handler(commands=['start'])
-    def start_welcome(message):
-        send_welcome(message, bot)
 
-    @bot.message_handler(func=lambda message: message.text.lower() in ['horror', 'comedy', 'action', 'drama', 'romance', 'thriller'])
-    @log_message
-    def genre_selected(message):
-        genre = message.text.lower()
-        movies = get_movies_by_genre(genre)
-        if movies:
-            response = f"Топ 5 фільмів жанру {genre.capitalize()}:\n"
-            for i, movie in enumerate(movies[:5], 1):
-                response += f"{i}. {movie}\n"
-            bot.send_message(message.chat.id, response)
-        else:
-            bot.send_message(message.chat.id, "Не вдалося отримати фільми.")
+maksym = Maksym("Максим", "Лебезун", 2008)
 
-    @bot.message_handler(func=lambda message: True)
-    @log_message
-    def handle_unknown(message):
-        handle_unknown_message(message, bot)
-
-    print("Бот запущено...")
-    bot.infinity_polling()
-
-if __name__ == "__main__":
-    main()
+print(maksym.get_course())
+print(maksym.get_name_list())
